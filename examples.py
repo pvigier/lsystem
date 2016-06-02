098e2a1595e6ce37d36822a7c1fd9b33e0bd3608
Pierre <pierre.vigier@ymail.com>
2016-06-02 15:08:24 +0200

    Add 3D plots

--- examples.py
+++ examples.py
@@ -1,11 +1,15 @@
 from grammar import *
-from tortoise2d import *
+from plot import *
+
+# Grammar
 
 def algae():
 	system = LSystem('A', {'A': 'AB', 'B': 'A'})
 	for _ in range(8):
 		print(system.state)
 		system.step()
+
+# 2D plots
 
 def koch():
 	system = LSystem('F', {'F': 'F+F-F-F+F'})
@@ -17,19 +21,33 @@ def tree():
 	system = LSystem('B', {'B': 'F/[+B]-B*'})
 	for _ in range(7):
 		system.step()
-	plot2d(system.state, 20, 2, 90, 'g')
+	plot2d(system.state, 20, 2, np.array([0, 1.]), 'g')
 
 def plant():
 	system = LSystem('X', {'X': 'F+[[X]-X]-F[-FX]+X', 'F': 'FF'})
 	for _ in range(5):
 		system.step()
-	plot2d(system.state, 25, 1, 50, 'g')
+	plot2d(system.state, 25, 1, rotate(np.array([1., 0]), 50), 'g')
 
 def liana():
 	system = LSystem('F', {'F': 'F[+F]F[-F]F'})
 	for _ in range(5):
 		system.step()
-	plot2d(system.state, 25, 1, 90, 'g')
+	plot2d(system.state, 25, 1, np.array([0, 1.]), 'g')
+
+# 3D plots
+
+def tree3d():
+	system = LSystem('B', {'B': 'F/[+B][-B][&B]^B*'})
+	for _ in range(6):
+		system.step()
+	plot3d(system.state, 20, 1.2, np.array([0, 0, 1.]), 'g')
+
+def pine3d():
+	system = LSystem('B', {'B': 'F/[+++B][---B][&&&B][^^^B]*F/[++B][--B][&&B][^^B]*F/B*'})
+	for _ in range(4):
+		system.step()
+	plot3d(system.state, 20, 2, np.array([0, 0, 1.]), 'g')
 
 if __name__ == '__main__':
-	tree()
+	pine3d()
---
+++ plot.py
@@ -0,0 +1,81 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from util import *
+
+# 2D
+
+def plot2d(string, alpha, delta=1, init_direction=np.array([1., 0]), color='k'):
+	plt.gca().set_aspect('equal', adjustable='box')
+	pos = np.zeros(2, dtype=np.float64)
+	direction = init_direction
+	saved_states = []
+	for x in string:
+		if x == 'F':
+			new_pos = pos + direction
+			plt.plot([pos[0], new_pos[0]], [pos[1], new_pos[1]], c=color)
+			pos = new_pos
+		elif x == '+':
+			direction = rotate(direction, alpha)
+		elif x == '-':
+			direction = rotate(direction, -alpha)
+		elif x == '*':
+			direction *= delta
+		elif x == '/':
+			direction /= delta
+		elif x == '|':
+			direction = rotate(direction, 180)
+		elif x == '[':
+			saved_states.append((pos, direction))
+		elif x == ']':
+			pos, direction = saved_states.pop()
+	plt.show()
+
+# 3D
+
+def axis_equal_3d(ax):
+    extents = np.array([getattr(ax, 'get_{}lim'.format(dim))() for dim in 'xyz'])
+    sz = extents[:,1] - extents[:,0]
+    centers = np.mean(extents, axis=1)
+    maxsize = max(abs(sz))
+    r = maxsize / 2
+    for ctr, dim in zip(centers, 'xyz'):
+        getattr(ax, 'set_{}lim'.format(dim))(ctr - r, ctr + r)
+
+def plot3d(string, alpha, delta=1, init_direction=np.array([1., 0, 0]), color='k'):
+	fig = plt.figure()
+	ax = fig.add_subplot(111, projection='3d')
+	pos = np.zeros(3, dtype=np.float64)
+	direction = init_direction
+	saved_states = []
+	for x in string:
+		if x == 'F':
+			new_pos = pos + direction
+			ax.plot([pos[0], new_pos[0]], [pos[1], new_pos[1]], [pos[2], new_pos[2]], c=color)
+			pos = new_pos
+		elif x == '+':
+			direction = rotate_x(direction, alpha)
+		elif x == '-':
+			direction = rotate_x(direction, -alpha)
+		elif x == '&':
+			direction = rotate_y(direction, alpha)
+		elif x == '^':
+			direction = rotate_y(direction, -alpha)
+		elif x == '<':
+			direction = rotate_z(direction, alpha)
+		elif x == '>':
+			direction = rotate_z(direction, -alpha)
+		elif x == '*':
+			direction = direction * delta
+		elif x == '/':
+			direction = direction / delta
+		elif x == '|':
+			direction = rotate_x(direction, 180)
+			direction = rotate_y(direction, 180)
+			direction = rotate_z(direction, 180)
+		elif x == '[':
+			saved_states.append((pos, direction))
+		elif x == ']':
+			pos, direction = saved_states.pop()
+	axis_equal_3d(ax)
+	plt.show()
